79d911a013d1cec4f24e6e95b4b20979dd24767c
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2019-09-05 15:00:17 -0600

    commit before 1:1

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,7 +1,20 @@
 /* Step 1: using axios, send a GET request to the following URL 
-           (replacing the palceholder with your Github name):
+           (replacing the placeholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios
+
+.get('https://api.github.com/users/ChrisRDaniels')
+.then(response => {
+  console.log(response);
+  // response.data.message.forEach(user => {
+  //   const UserCard = newUser(user);
+  //   entrypoint.appendChild(UserCard);
+  // });
+})
+.catch(error =>{
+  console.log('Data not returned', error);
+});
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -46,6 +59,34 @@ const followersArray = [];
 
 */
 
+
+function UserCard(avatar_url) {
+  const 
+    newUser = document.createElement("div"),
+    newImage = document.createElement("img"),
+    newName = document.createElement("h3"),
+    newUsername = document.createElement("p"),
+    newLocation = document.createElement('p'),
+    newProfile = document.createElement('p'),
+    newFollowers = document.createElement('p'),
+    newFollowing = document.createElement('p')
+    newBio = document.createElement('p');
+
+
+  breed.textContent = "Breed: Mastiff";
+  newImage.src = imgUrl;
+  newImage.classList.add("dog-image");
+  newCard.classList.add("dog-card");
+  newCard.appendChild(newImage);
+  newCard.appendChild(breed);
+
+  // add this functionality later
+  newCard.addEventListener("click", () => {
+    newCard.classList.toggle("selected");
+  });
+
+  return newCard;
+}
 /* List of LS Instructors Github username's: 
   tetondan
   dustinmyers
